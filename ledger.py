5c459c9bc061aa3d74e484fc651903a761423d8f
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2022-06-24 17:55:00 -0300

    Update ledger.py

--- ledger.py
+++ ledger.py
@@ -9,44 +9,58 @@ IROHA_PORT = os.getenv('IROHA_PORT', '50051')
 ADMIN_ACCOUNT_ID = os.getenv('ADMIN_ACCOUNT_ID', 'admin@test')
 ADMIN_PRIVATE_KEY = os.getenv('ADMIN_PRIVATE_KEY', 'f101537e319568c765b2cc89698325604991dca57b9716b58016b253506cab70')
 
-
 class Ledger:
-    def __init__(self, sawmills, l):
-        self.domain_name = "trade"
+    def __init__(self):
+        self.__private_key = IrohaCrypto.private_key()
+        self.public_key = IrohaCrypto.derive_public_key(self.__private_key)
         self.admin_private_key = ADMIN_PRIVATE_KEY
         self.iroha = Iroha(ADMIN_ACCOUNT_ID)
         self.net = IrohaGrpc('{}:{}'.format(IROHA_HOST_ADDR, IROHA_PORT))
-        self.sawmills = sawmills
-
-        self.woods = list(map(config.to_lower_case_only_letters, config.woods))  # uses as assets
-        self.commands = [
-            self.iroha.command('CreateDomain', domain_id=self.domain_name, default_role='user'),
-            *[self.iroha.command('CreateAsset', asset_name=wood,
-                                 domain_id=self.domain_name, precision=0)
-              for wood in self.woods]
-        ]
-        tx = IrohaCrypto.sign_transaction(
-            self.iroha.transaction(self.commands), self.admin_private_key)
-        print(self.send_transaction_and_log_status(tx))
-        self.get_admin_details()
-        tx = self.iroha.transaction(
-            [self.iroha.command('AddAssetQuantity', asset_id=f'{wood}#{self.domain_name}',
-                                amount=str(10000//l))
-             for wood in self.woods]
-        )
-        IrohaCrypto.sign_transaction(tx, self.admin_private_key)
-        print(self.send_transaction_and_log_status(tx))
-        self.get_admin_details()
-
+        
     def send_transaction_and_log_status(self, transaction):
         hex_hash = binascii.hexlify(IrohaCrypto.hash(transaction))
         print('Transaction hash = {}, creator = {}'.format(
             hex_hash, transaction.payload.reduced_payload.creator_account_id))
         self.net.send_tx(transaction)
-        return list(self.net.tx_status_stream(transaction))
-
+        return list(self.net.tx_status_stream(transaction))    
+        
     def get_admin_details(self):
-        print('admin details...')
+        result_dict = {}
+        query = self.iroha.query('GetAccountDetail', account_id=f'admin@test')
+        IrohaCrypto.sign_query(query, self.admin_private_key)
+
+        response = self.net.send_query(query)
+        data = response.account_detail_response
+        print(data)
+        print('Account id = {}, details = {}'.format('admin@test', data.detail))
+        result_dict['admin@test'] = data.detail
+        return result_dict
+
+    def get_user_details(self):
+        result_dict = {}
+        query = self.iroha.query('GetAccountDetail', account_id=f'userone@domain')
+        IrohaCrypto.sign_query(query, self.admin_private_key)
+
+        response = self.net.send_query(query)
+        data = response.account_detail_response
+        print(data)
+        print('Account id = {}, details = {}'.format('userone@domain', data.detail))
+        result_dict['userone@domain'] = data.detail
+        return result_dict
+
+    def get_user_account_assets(self):
+        result_dict = {}
+        query = self.iroha.query('GetAccountAssets', account_id=f'userone@domain')
+        IrohaCrypto.sign_query(query, self.admin_private_key)
+
+        response = self.net.send_query(query)
+        data = response.account_assets_response.account_assets
+        for asset in data:
+            print('Asset id = {}, balance = {}'.format(asset.asset_id, asset.balance))
+            result_dict[asset.asset_id] = asset.balance
+        return result_dict
+
+    def get_admin_account_assets(self):
         result_dict = {}
         query = self.iroha.query('GetAccountAssets', account_id=f'admin@test')
         IrohaCrypto.sign_query(query, self.admin_private_key)
@@ -54,38 +68,21 @@ class Ledger:
         response = self.net.send_query(query)
         data = response.account_assets_response.account_assets
         for asset in data:
-            print('Asset id = {}, balance = {}'.format(
-                asset.asset_id, asset.balance))
+            print('Asset id = {}, balance = {}'.format(asset.asset_id, asset.balance))
             result_dict[asset.asset_id] = asset.balance
         return result_dict
 
-    def init_ledger(self):
-        print('init ledger...')
+    def set_key_pair_to_userone(self, value_input):
+        tx = self.iroha.transaction([self.iroha.command('SetAccountDetail',
+        account_id='userone@domain', key='hash_1', value = value_input)])
+        IrohaCrypto.sign_transaction(tx, self.admin_private_key)
+        print(self.send_transaction_and_log_status(tx))
+        
+    def create_account(self, username_input, domain_input):
+        print('Create Account...')
         tx = self.iroha.transaction([
-            self.iroha.command('CreateAccount', account_name=sawmill.account_name, domain_id=sawmill.domain,
-                               public_key=sawmill.public_key)
-            for sawmill in self.sawmills
+            self.iroha.command('CreateAccount', account_name = username_input, domain_id = domain_input,
+                               public_key=self.public_key)
         ])
         IrohaCrypto.sign_transaction(tx, self.admin_private_key)
         print(self.send_transaction_and_log_status(tx))
-
-        print("=" * 20)
-        tx_commands = []
-        for i in self.sawmills:
-            for j in self.woods:
-                tx_commands.append(self.iroha.command('TransferAsset', src_account_id='admin@test',
-                                                      dest_account_id=f'{i.account_name}@{self.domain_name}',
-                                                      asset_id=f'{j}#{self.domain_name}',
-                                                      amount=str(randint(1, 100))))
-        tx = self.iroha.transaction(tx_commands)
-        IrohaCrypto.sign_transaction(tx, self.admin_private_key)
-        print(self.send_transaction_and_log_status(tx))
-        self.get_admin_details()
-        print(self.get_accounts_info())
-        print("=" * 20)
-
-    def get_accounts_info(self):
-        result_dict = {}
-        for i in self.sawmills:
-            result_dict[i.account_name] = i.get_woods_balance()
-        return result_dict
